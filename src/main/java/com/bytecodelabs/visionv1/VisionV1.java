f5d732ef7f8507d26f9900fe980227b9fd369356
GavinReid1337 <gavinreid@null.net>
2024-06-11 16:32:03 -0400

    cool

---
+++ src/main/java/com/bytecodelabs/visionv1/VisionV1.java
@@ -0,0 +1,17 @@
+package com.bytecodelabs.visionv1;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VisionV1 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
